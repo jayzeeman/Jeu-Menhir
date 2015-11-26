06faed6c5c93ae5d04c6046b87c0dcd8bd7fd79f
perrotbryan <perrot.bryan@gmail.com>
2015-11-26 13:53:57 +0100

    Update StrategieOffensive.java

--- Code/Jeu-Menhir/src/StrategieOffensive.java
+++ Code/Jeu-Menhir/src/StrategieOffensive.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 
 public class StrategieOffensive implements Strategie {
 	private JoueurVirtuel joueurVirtuel;
@@ -7,10 +8,24 @@ public class StrategieOffensive implements Strategie {
 	}
 	
 	public CarteIngredient jouerIngredient() {
-		return null;
+		
 	}
 	
 	public CarteAllie jouerAllie() {
+		if(joueurVirtuel.getCarteAllie().getNom() == "Taupe géante") {
+			return joueurVirtuel.getCarteAllie();
+		}
 		return null;
+	}
+	
+	private CarteIngredient getBestCarteFor(int saison) {
+		Carte best = null;
+		for(Iterator it = joueurVirtuel.getCartesIngredient.iterator();it.hasNext();) {
+			Carte carte = (Carte)it.next();
+			if(best==null || carte.getForce()[saison][2]) {
+				best = carte;
+			}
+		}
+		return best;
 	}
 }
