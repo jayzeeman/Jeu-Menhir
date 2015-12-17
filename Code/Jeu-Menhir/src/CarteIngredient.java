430246a583242ec638b81ae78768da68ff842a22
perrotbryan <perrot.bryan@gmail.com>
2015-12-17 13:21:09 +0100

    Déplacement des constantes d'action

--- Code/Jeu-Menhir/src/CarteIngredient.java
+++ Code/Jeu-Menhir/src/CarteIngredient.java
@@ -1,5 +1,9 @@
 
 public class CarteIngredient extends Carte {
+	public final static byte ACTION_GEANT = 0;
+	public final static byte ACTION_ENGRAIS = 1;
+	public final static byte ACTION_FARFADETS = 2;
+	
 	private int[][] force;
 
 	public CarteIngredient(String nom, int[][] force) {
@@ -13,8 +17,8 @@ public class CarteIngredient extends Carte {
 	
 	public String toString() {
 		String string = this.nom + " : \n ";
-		string += "\t Printemps   Été   Automne   Hiver \n";
-		string += "Géant \t\t" + force[0][0]  + "\t" + force[0][1] + "\t" + force[0][2] + "\t" + force[0][3] + "\n";
+		string += "\t Printemps   Ã‰tÃ©   Automne   Hiver \n";
+		string += "GÃ©ant \t\t" + force[0][0]  + "\t" + force[0][1] + "\t" + force[0][2] + "\t" + force[0][3] + "\n";
 		string += "Engrais \t" + force[1][0]  + "\t" + force[1][1] + "\t" + force[1][2] + "\t" + force[1][3] + "\n";
 		string += "Farfadets \t" + force[2][0]  + "\t" + force[2][1] + "\t" + force[2][2] + "\t" + force[2][3] + "\n";
 		return string;
