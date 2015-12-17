77daf77e30ed232def3431e862e85dd0cad0d413
perrotbryan <perrot.bryan@gmail.com>
2015-12-17 13:20:46 +0100

    Déplacement des constantes d'action

--- Code/Jeu-Menhir/src/Carte.java
+++ Code/Jeu-Menhir/src/Carte.java
@@ -1,8 +1,5 @@
 
 public abstract class Carte {
-	public final static byte ACTION_GEANT = 0;
-	public final static byte ACTION_ENGRAIS = 1;
-	public final static byte ACTION_FARFADETS = 2;
 	
 	protected String nom;
 
